8ceb176041ed1a04d4a185e6cb1a7951bd523c42
ubuntu_test <fortumusara@gmail.com>
2025-04-26 13:44:44 +0200

    Fix CodeBuild environment variables syntax for Terraform

--- main.tf
+++ main.tf
@@ -68,25 +68,37 @@ resource "aws_iam_role_policy_attachment" "codebuild_policy_attach" {
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
 }
 
-# CodeBuild Project
-resource "aws_codebuild_project" "build_project" {
-  name          = "margin-assurance-build"
-  service_role  = aws_iam_role.codebuild_role.arn
 
-  artifacts {
-    type = "CODEPIPELINE"
-  }
+
+#CodeBuild Project
+resource "aws_codebuild_project" "build_project" {
+  name         = "margin-assurance-build"
+  service_role = aws_iam_role.codebuild_role.arn
 
   environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:7.0" # Latest image
-    type                        = "LINUX_CONTAINER"
-    privileged_mode             = true
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:5.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = true
+
+    environment_variable {
+      name  = "AWS_REGION"
+      value = var.aws_region
+    }
+
+    environment_variable {
+      name  = "AWS_DEFAULT_REGION"
+      value = var.aws_region
+    }
   }
 
   source {
+    type      = "CODEPIPELINE"
+    buildspec = "buildspec.yml"
+  }
+
+  artifacts {
     type = "CODEPIPELINE"
-    buildspec = "buildspec.yml" # Assumes you have a buildspec.yml at repo root
   }
 }
 
